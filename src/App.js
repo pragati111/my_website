0a427c09a3096f0a49cfb42d050ead4def7af10e
Pragati Agarwal <pragatinov@gmail.com>
2026-03-02 18:31:29 +0530

    extra imports removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Hero from './components/Hero/Hero';
 import Header from './components/Header/Header';
 
 // Product pages
